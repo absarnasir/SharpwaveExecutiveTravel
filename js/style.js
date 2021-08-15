9ba8f63248bd4e1e06c9847688a38dc026c25779
absarnasir <absarnasir3@gmail.com>
2021-08-15 07:13:27 -0700

    hi
    thanks

---
+++ js/style.js
@@ -0,0 +1,38 @@
+
+var load=document.getElementById("loading");
+function loadfun(){
+    load.style.display='none';
+}
+document.addEventListener("DOMContentLoaded", function(){
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 300) {
+          document.getElementById('navbar_top').classList.add('fixed-top');
+          // add padding top to show content behind navbar
+          navbar_height = document.querySelector('.navbar').offsetHeight;
+          document.body.style.paddingTop = navbar_height + 'px';
+        } else {
+          document.getElementById('navbar_top').classList.remove('fixed-top');
+           // remove padding top from body
+          document.body.style.paddingTop = '0';
+        } 
+    });
+  }); 
+  
+  $('.carousel.carousel-multi-item.v-2 .carousel-item').each(function(){
+    var next = $(this).next();
+    if (!next.length) {
+      next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+  
+    for (var i=0;i<3;i++) {
+      next=next.next();
+      if (!next.length) {
+        next = $(this).siblings(':first');
+      }
+      next.children(':first-child').clone().appendTo($(this));
+    }
+  });
+
+
+  AOS.init();
